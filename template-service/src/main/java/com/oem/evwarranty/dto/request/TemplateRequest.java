e38533c8b06c472d1cf4d54f39e76cddce581e25
TAN DAT <dattt1848@ut.edu.vn>
2025-10-07 15:24:47 +0700

    Mo hinh service mau

---
+++ template-service/src/main/java/com/oem/evwarranty/dto/request/TemplateRequest.java
@@ -0,0 +1,11 @@
+package com.oem.evwarranty.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TemplateRequest {
+}
