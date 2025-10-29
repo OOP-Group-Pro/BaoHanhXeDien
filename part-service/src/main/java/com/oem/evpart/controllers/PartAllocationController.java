369a85b9b98a82cb749bffb159cf4e5358ae7e70
Nguyen Minh Tuan Khanh <khanhblue06@gmail.com>
2025-10-29 22:16:47 +0700

    Up thêm api feign cho vehicle-service

--- part-service/src/main/java/com/oem/evpart/controllers/PartAllocationController.java
+++ part-service/src/main/java/com/oem/evpart/controllers/PartAllocationController.java
@@ -1,8 +1,11 @@
 package com.oem.evpart.controllers;
 
+import com.oem.evpart.dto.request.DecrementStockRequest;
 import com.oem.evpart.dto.request.PartAllocationRequest;
 import com.oem.evpart.dto.response.PartAllocationResponse;
+import com.oem.evpart.dto.response.PartInventoryResponse;
 import com.oem.evpart.services.PartAllocationService;
+import com.oem.evpart.services.PartInventoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,10 +21,22 @@ public class PartAllocationController {
 
     private final PartAllocationService allocationService;
 
+    private final PartInventoryService inventoryService;
     @PostMapping
     public ResponseEntity<PartAllocationResponse> createAllocation(@Valid @RequestBody PartAllocationRequest request) {
         PartAllocationResponse response = allocationService.createAllocation(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // --- API MỚI ĐỂ TRỪ KHO SAU KHI SỬ DỤNG ---
+    /**
+     * Endpoint này được gọi bởi service khác (vd: VehicleService)
+     * sau khi một phụ tùng đã được lắp đặt thành công, để trừ tồn kho.
+     */
+    @PostMapping("/decrement")
+    public ResponseEntity<PartInventoryResponse> decrementStockAfterUsage(
+            @Valid @RequestBody DecrementStockRequest request) {
+        return ResponseEntity.ok(inventoryService.decrementStock(request));
     }
 
     @GetMapping("/{id}")
