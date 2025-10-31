d7371cb8634ddffa199d2a22a69d6d30b322acb4
Nguyen Minh Tuan Khanh <khanhblue06@gmail.com>
2025-10-31 23:37:15 +0700

    Up Dockerfile

--- part-service/src/main/java/com/oem/evpart/config/AuthenticationFilter.java
+++ part-service/src/main/java/com/oem/evpart/config/AuthenticationFilter.java
@@ -47,7 +47,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         try {
             log.debug("Đang xác thực User ID: {}", userId);
             // Kích hoạt Feign Client gọi sang User-Service (8001)
-            UserResponseDTO user = userServiceClient.getUser(Long.parseLong(userId));
+            UserResponseDTO user = userServiceClient.getUserById(Long.parseLong(userId)).getBody();
 
             if (user != null && user.getRoles() != null) {
                 // 3. TẠO AUTHENTICATION THÀNH CÔNG
